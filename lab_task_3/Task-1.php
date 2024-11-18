ef278f4f85ff5bb183b3ffb283b0c65477b9fb84
Himel7136 <141748755+Himel7136@users.noreply.github.com>
2024-11-18 13:20:13 +0600

    Add files via upload

---
+++ lab_task_3/Task-1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $length=472;
+    $width=78;
+
+    $area=$length*$width;
+    $perimeter=2*($length+$width);
+
+    echo "The Length of Rectangle = "."$length<br>";
+    echo"The Width of Rectangle = ". "$width<br>";
+
+    echo"The Area of Rectangle = "."$area<br>";
+    echo"The Perimeter of Rectangle = "."$perimeter";
+
+?>
